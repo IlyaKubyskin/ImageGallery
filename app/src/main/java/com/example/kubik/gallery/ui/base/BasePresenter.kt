12d57139b7209a270c4106ba549e8dca940f5a8c
IlyaKubyshkin <ilkubik@yandex.ru>
2018-11-04 19:45:20 +0300

    fix

--- app/src/main/java/com/example/kubik/gallery/ui/base/BasePresenter.kt
+++ app/src/main/java/com/example/kubik/gallery/ui/base/BasePresenter.kt
@@ -10,24 +10,20 @@ import java.lang.Exception
 
 open class BasePresenter<T : MvpView> : MvpPresenter<T>() {
 
-    private var job: Job? = null
+    private var jobs = mutableListOf<Job>()
 
     fun startLoadingJob(block: suspend () -> Unit) {
-        if (job != null) {
-            job?.cancel()
-            job = null
-        }
-        job = GlobalScope.launch(Dispatchers.Main) {
+        jobs.add(GlobalScope.launch(Dispatchers.Main) {
             try {
                 block.invoke()
             } catch (e: Exception) {
                 e.printStackTrace()
             }
-        }
+        })
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        job?.cancel()
+        jobs.forEach { it.cancel() }
     }
 }
